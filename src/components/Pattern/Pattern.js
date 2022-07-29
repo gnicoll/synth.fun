27a7a8f949f24241d442d53177b014c19d5598b1
Glenn <glennnicoll@gmail.com>
2022-07-29 16:40:42 +1000

    pattern

--- src/components/Pattern/Pattern.js
+++ src/components/Pattern/Pattern.js
@@ -1,14 +1,35 @@
 import style from './Pattern.css';
 import { useSynth } from '../../context/SynthContext';
+import { useEffect, useState } from 'react';
 
 const Pattern = () => {
   const { loop, controls, dispatch } = useSynth();
   const handleClick = (PatternNum) => {
   }
+
   
   return (
     <div className="arp_patterns" >
       <div className='arp_pattern'>
+        <div className={'arp_pattern_patternvisual arp_pattern_patternvisual_playingentry'+controls.patternIndex}>
+            {controls?.pattern?.map((entry, index) => 
+            {
+              //(patternMax - entry) 100(patternMax - patternMin) 
+              let prevEntry = index > 0 ? controls.pattern[index - 1] : controls.pattern[index];
+              let l = (100 * (Math.max(...controls.pattern) - prevEntry)) / ((Math.max(...controls.pattern) - Math.min(...controls.pattern) +1));
+              let r = (100 * (Math.max(...controls.pattern) - entry)) / ((Math.max(...controls.pattern) - Math.min(...controls.pattern) +1));
+              return <div 
+                      className={'arp_pattern_patternvisual_entry'}             
+                      style = {
+                        {
+                          'clipPath': 'polygon(0 '+l+'%, 100% '+r+'%, 100% 100%, 0% 100%)',
+                        }
+                      } 
+                      key={index}>
+                    </div>
+            }
+            )}
+        </div>
         <div className='arp_pattern_image' >
 
         </div>
@@ -26,24 +47,6 @@ const Pattern = () => {
           </div>
 
         </div>
-      </div>
-      <div>
-        <div className=''>{controls.patternIndex}</div>
-        {controls?.pattern?.map((entry, index) => 
-        {
-          
-          return <span 
-                  style = {
-                    {
-                      'clip-path': 'polygon(0 16%, 100% 30%, 100% 100%, 0% 100%)',
-                      backgroundColor: 'red'
-                    }
-                  } 
-                  key={index}>
-                    {entry},
-                  </span>
-        }
-      )}
       </div>
     </div>
   )
